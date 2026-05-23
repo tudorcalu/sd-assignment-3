98b2588c8d08b60e3aa2b04a8cde64c2ce630a87
tudorcalu <tudycalu@gmail.com>
2026-05-23 19:41:54 +0300

    route movie and recommendation traffic through gateway

---
+++ gateway-service/src/main/java/org/example/assignment3/gateway/GatewayServiceApplication.java
@@ -0,0 +1,12 @@
+package org.example.assignment3.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+}
